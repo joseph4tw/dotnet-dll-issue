0867d07d7f7c5648c467ee76a4dcfd114958f860
Joseph Serido <joseph.serido@gmail.com>
2020-08-13 17:40:02 -0400

    Add project files.

---
+++ dll-issue/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace dll_issue
+{
+    public class Class1
+    {
+        public void WriteMe()
+        {
+            System.Console.WriteLine(RuntimeInformation.FrameworkDescription);
+        }
+    }
+}
